0b9e5503bd8877131dda209f047257ab643fa723
Vinaytitarmare <146834025+Vinaytitarmare@users.noreply.github.com>
2024-08-29 22:07:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu_button = document.querySelector("#menu_button");
+let sidebar = document.querySelector("#sidebar"); 
+
+menu_button.addEventListener("click", () => {
+    sidebar.style.display = 'block';
+    
+});
